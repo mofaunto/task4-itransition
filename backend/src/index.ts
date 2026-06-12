a9668cf938f814d6d8251a35b34f28ea770cfae9
Khozhimatov Muzaffar <xjmuzaffar@gmail.com>
2026-06-12 21:52:57 +0900

    init: setup the initial backend with express

---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// CORS for frontend
+app.use(cors({
+    origin: process.env.NODE_ENV === 'production' 
+        ? 'https://frontend.com' 
+        : 'http://localhost:5173',
+    credentials: true
+}));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
+
+export default app;
