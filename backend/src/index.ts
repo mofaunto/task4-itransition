c1415255f032e758fc7c5b433385d16f9b65e93d
Khozhimatov Muzaffar <xjmuzaffar@gmail.com>
2026-06-17 01:15:56 +0900

    refactor: frontend and backend env and cors updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 // CORS for frontend
 app.use(cors({
     origin: process.env.NODE_ENV === 'production' 
-        ? 'https://frontend.com' 
+        ? process.env.FRONTEND_URL 
         : 'http://localhost:5173',
     credentials: true
 }));
